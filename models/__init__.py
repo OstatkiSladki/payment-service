02fc03eee18d0860f245a43ee0bd30e8616dcec8
kuyltep <vladpetlu91@gmail.com>
2026-03-15 14:27:28 +0300

    init project structure

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+from models.payment import Payment, PaymentMethod, PaymentStatus
+from models.promo_code import PromoCode
+from models.promo_code_usage import PromoCodeUsage
+
+__all__ = [
+    "Payment",
+    "PaymentMethod",
+    "PaymentStatus",
+    "PromoCode",
+    "PromoCodeUsage",
+]
